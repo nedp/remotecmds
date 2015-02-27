ca40092167f2766b83b3e0f9223ba3e756669bd8
Ned Pummeroy <nedpummeroy@gmail.com>
2015-02-27 18:04:52 +1100

    added output channels to say_command_test

--- say/say_command_test.go
+++ say/say_command_test.go
@@ -10,12 +10,14 @@ import (
 const shortDuration = time.Second
 
 func TestCommand(t *testing.T) {
-	command.New(NewSequence(Params{"This is the Integration test! Will it pass?"})).Run()
+	command.New(NewSequence(Params{"This is the Integration test! Will it pass?"}),
+		make(chan string)).Run(make(chan string))
 }
 
 func TestKill(t *testing.T) {
-	c := command.New(NewSequence(Params{"This should be cut off now; before it finishes."}))
-	go c.Run()
+	c := command.New(NewSequence(Params{"This should be cut off now; before it finishes."}),
+		make(chan string))
+	go c.Run(make(chan string))
 	go func() {
 		time.Sleep(shortDuration)
 		c.Kill()
